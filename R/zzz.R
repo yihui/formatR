33ac031105d4b3cf13695ee8e797036cd7f1e8de
Yihui Xie <xie@yihui.name>
2010-04-19 19:57:30 +0000

    fixed the dependence problems; v0.1-4 ready

--- R/zzz.R
+++ R/zzz.R
@@ -1,21 +1,24 @@
 .First.lib <- function(lib, pkg) {
-    if (is.null(getOption("guiToolkit"))) {
-        if (require("gWidgetsRGtk2"))
-            options(guiToolkit = "RGtk2")
-        else {
-            if (require("gWidgetsrJava"))
-                options(guiToolkit = "rJava")
-            else {
-                if (require("gWidgetstcltk"))
-                  options(guiToolkit = "tcltk")
+    if (interactive()) {
+        if (is.null(getOption("guiToolkit"))) {
+            pa = FALSE
+            for (p in c("RGtk2", "rJava", "tcltk")) {
+                if (require(paste("gWidgets", p, sep = ""), character.only = TRUE)) {
+                  options(guiToolkit = p)
+                  pa = TRUE
+                  formatR()
+                  break
+                }
                 else {
-                  packageStartupMessage("You need to install one of packages 'gWidgetsRGtk2', 'gWidgetsrJava' \n",
-                    "    and 'gWidgetstcltk' to create the GUI.\n",
-                    "See ?formatR for more infomation.")
+                  packageStartupMessage("Package ", paste("gWidgets",
+                    p, sep = ""), " is not available...")
                 }
             }
+            if (!pa)
+                packageStartupMessage("You need to install one of packages 'gWidgetsRGtk2', 'gWidgetsrJava' \n",
+                  "    and 'gWidgetstcltk' to create the GUI.\n",
+                  "For more information, see ?formatR")
         }
+        else formatR()
     }
-    if (interactive())
-        formatR()
 }
