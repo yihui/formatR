d82be708db70c0a237540f5537e665d4f945a6e9
Yihui Xie <xie@yihui.name>
2011-01-10 21:05:38 -0600

    must use .onLoad instead of .First.lib, since formatR has a NAMESPACE now; cleaned start up message

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,6 @@
-.First.lib <- function(lib, pkg) {
+.onLoad <- function(lib, pkg) {
     if (interactive()) {
-        if (is.null(getOption("guiToolkit"))) {
-            packageStartupMessage("You need to install/load one of packages 'gWidgetsRGtk2',\n",
-                                  "   'gWidgetsrJava', 'gWidgetstcltk' or 'gWidgetsQt'\n",
-                                  " and set options(guiToolkit = 'XXX') to create the GUI.\n",
-                                  "For more information, see ?formatR")
-
-        } else formatR(getOption("guiToolkit"))
+        if (!is.null(getOption("guiToolkit")))
+            try(formatR(getOption("guiToolkit")))
     }
 }
