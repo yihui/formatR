f38f5224dacbf7494a736bb7f6f6aff60788b322
Yihui Xie <xie@yihui.name>
2011-05-19 17:04:41 -0500

    fix the script to cheat Sweave

--- inst/doc/clean-up.R
+++ inst/doc/clean-up.R
@@ -1,11 +1,14 @@
 #!/usr/bin/env Rscript
 
 ## deal with LyX filename mangling
-x = readLines('formatR.tex')
-idx = grep('\\\\documentclass', x)
-if (idx > 1) x = x[-(1:(idx-1))]
-idx = grep('\\\\bibliography|\\\\includegraphics', x)
-x[idx] = sub('\\{.*formatR_inst_doc_', '{', x[idx])
-writeLines(x, 'formatR.tex')
-file.rename('formatR.tex', 'formatR.Rnw')
+if (file.exists('formatR.tex')) {
+    x = readLines('formatR.tex')
+    idx = grep('\\\\documentclass', x)
+    if (idx > 1) x = x[-(1:(idx-1))]
+    idx = grep('\\\\bibliography|\\\\includegraphics', x)
+    x[idx] = sub('\\{.*formatR_inst_doc_', '{', x[idx])
+    writeLines(x, 'formatR.tex')
+    file.rename('formatR.tex', 'formatR.Rnw')
+}
+unlink(sprintf('formatR.%s', c('aux', 'log', 'out', 'pdf')))
 ## now we can cheat Sweave :-)
