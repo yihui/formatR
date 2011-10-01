65944f29300021585c472fb75f5bad61f9d40ae1
Yihui Xie <xie@yihui.name>
2011-10-01 13:50:44 -0500

    add an argument 'envir' in tidy.eval()

--- R/eval.R
+++ R/eval.R
@@ -9,6 +9,10 @@
 ##' @param file the file to write by \code{\link[base]{cat}}; by
 ##' default the output is printed on screen
 ##' @param prefix the prefix to mask the output
+##' @param envir the environment in which to evaluate the code (by
+##' default the global environment; if we do not want to mess up with
+##' the global environment, we can set \code{envir = NULL} or
+##' \code{envir = new.env()})
 ##' @return Evaluated R code with corresponding output (printed on
 ##' screen or written in a file).
 ##' @author Yihui Xie <\url{http://yihui.name}>
@@ -20,11 +24,11 @@
 ##'
 ##' ## evaluate a file
 ##' tidy.eval(source = file.path(system.file(package = "stats"), "demo", "nlm.R"), keep.blank.line = TRUE)
-tidy.eval = function(source = 'clipboard', ..., file = "", prefix = "## ") {
+tidy.eval = function(source = 'clipboard', ..., file = "", prefix = "## ", envir = .GlobalEnv) {
     txt = tidy.source(source, ..., output = FALSE)$text.mask
     for(i in 1:length(txt)) {
         cat(unmask.source(txt[i]), '\n', file = file, append = TRUE)
-        res = capture.output(eval(parse(text = txt[i])))
+        res = capture.output(eval(parse(text = txt[i]), envir = envir))
         if (length(res)) {
             cat(paste(prefix, res, sep = ''), sep = '\n', file = file, append = TRUE)
             cat('\n', file = file, append = TRUE)
