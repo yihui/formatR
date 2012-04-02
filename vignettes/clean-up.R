490a65c78bb007fd8dbbac89b87d5a9b03b719a9
Yihui Xie <xie@yihui.name>
2012-04-02 01:08:00 -0500

    do not need to clean up lyx file; will use knitr

--- vignettes/clean-up.R
+++ vignettes/clean-up.R
@@ -4,15 +4,3 @@
 library(tweakr)
 write_citation(c('Rd2roxygen', 'knitr', 'formatR', 'pgfSweave'), file = 'formatR.bib')
 
-## deal with LyX filename mangling
-if (file.exists('formatR.tex')) {
-    x = readLines('formatR.tex')
-    idx = grep('\\\\documentclass', x)
-    if (idx > 1) x = x[-(1:(idx-1))]
-    idx = grep('\\\\bibliography|\\\\includegraphics', x)
-    x[idx] = sub('\\{.*formatR_vignettes_', '{', x[idx])
-    writeLines(x, 'formatR.tex')
-    file.rename('formatR.tex', 'formatR.Rnw')
-}
-unlink(sprintf('formatR.%s', c('aux', 'log', 'out', 'pdf')))
-## now we can cheat Sweave :-)
