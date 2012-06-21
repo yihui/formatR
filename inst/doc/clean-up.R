028540f13200f0f9027ed8a456c20b4da659f948
Yihui Xie <xie@yihui.name>
2012-06-21 16:44:39 -0400

    use knitr instead of tweakr to write bib

--- inst/doc/clean-up.R
+++ inst/doc/clean-up.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
 ## automatic bib generation
-library(tweakr)
-write_citation(c('Rd2roxygen', 'knitr', 'formatR', 'pgfSweave'), file = 'formatR.bib')
+library(knitr)
+write_bib(c('Rd2roxygen', 'knitr', 'formatR', 'pgfSweave'), file = 'formatR.bib')
 
