4584ed8af7438f85c6264783d20f9b14c3eed4c0
Yihui Xie <xie@yihui.name>
2012-10-02 17:40:15 -0500

    fixed #23: use global variables tidy.opts and font.attr instead of attaching them to the tags of the text input

--- R/GUI.R
+++ R/GUI.R
@@ -54,8 +54,8 @@ tidy.gui = function(guiToolkit = 'RGtk2') {
   
   txt = gtext(container = g1, wrap = FALSE, 
               font.attr = c(family = "monospace", size = "medium"), expand = TRUE)
-  tag(txt, "font.attr") = c(family = "monospace", size = "medium", weight = "light")
-  tag(txt, "tidy.opt") = list(keep.comment = TRUE, keep.blank.line = TRUE,
+  font.attr = c(style = "normal", size = "medium", weight = "light")
+  tidy.opts = list(keep.comment = TRUE, keep.blank.line = TRUE,
                               keep.space = FALSE, replace.assign = FALSE, width.cutoff = 60)
   gbutton("Open", container = g2, handler = function(h, ...) {
     s = gfile("Open R Source")
@@ -66,16 +66,15 @@ tidy.gui = function(guiToolkit = 'RGtk2') {
     }
   })
   gbutton("Convert", container = g2, handler = function(h, ...) {
-    tidy.opt = tag(txt, "tidy.opt")
     src = svalue(txt)
     Encoding(src) = 'UTF-8'
     src = unlist(strsplit(src, '\n', fixed = TRUE))
     text.tidy = tidy.source(text = src,
-                            keep.comment = tidy.opt$keep.comment,
-                            keep.blank.line = tidy.opt$keep.blank.line,
-                            keep.space = tidy.opt$keep.space,
-                            replace.assign = tidy.opt$replace.assign,
-                            width.cutoff = tidy.opt$width.cutoff,
+                            keep.comment = tidy.opts$keep.comment,
+                            keep.blank.line = tidy.opts$keep.blank.line,
+                            keep.space = tidy.opts$keep.space,
+                            replace.assign = tidy.opts$replace.assign,
+                            width.cutoff = tidy.opts$width.cutoff,
                             output = FALSE)$text.tidy
     Encoding(text.tidy) = 'UTF-8'
     enc = options(encoding = "UTF-8")
@@ -103,20 +102,18 @@ tidy.gui = function(guiToolkit = 'RGtk2') {
       w = gwindow("Font Specification", )
       g = ggroup(horizontal = FALSE, container = w)
       tbl = glayout(container = g, expand = TRUE, spacing = 0)
-      ft = tag(txt, "font.attr")
       tbl[1, 1, expand = TRUE] = (gf.size <- gframe("Size", container = tbl))
-      r.size = gradio(tag(txt)$tags$sizes, 
-                      which(ft["size"] == tag(txt)$tags$sizes), 
+      r.size = gradio(gui.sizes,
+                      which(font.attr['size'] == gui.sizes),
                       horizontal = TRUE, container = gf.size)
       tbl[2, 1, expand = TRUE] = (gf.weight <- gframe("Weight", container = tbl))
-      r.weight = gradio(tag(txt)$tags$weight, 
-                        which(ft["weight"] == tag(txt)$tags$weight), 
+      r.weight = gradio(gui.weights,
+                        which(font.attr['weight'] == gui.weights),
                         horizontal = TRUE, container = gf.weight)
       g1 = ggroup(container = g)
       b.ok = gbutton("OK", container = g1, handler = function(h, ...) {
-        ft = c(family = "monospace", size = svalue(r.size), weight = svalue(r.weight))
-        font(txt) = ft
-        tag(txt, "font.attr") = ft
+        font.attr <<- c(family = "monospace", size = svalue(r.size), weight = svalue(r.weight))
+        font(txt) = font.attr
         dispose(w)
       })
       b.cancel = gbutton("Cancel", container = g1, handler = function(h, ...) {
@@ -128,33 +125,32 @@ tidy.gui = function(guiToolkit = 'RGtk2') {
     w = gwindow("Preferences")
     g = ggroup(horizontal = FALSE, container = w)
     tbl = glayout(container = g, expand = TRUE, spacing = 0)
-    tidy.opt = tag(txt, "tidy.opt")
     tbl[1, 1, expand = TRUE] = (gf.kc <- gframe("Keep Comments?", container = tbl))
-    r.kc = gradio(c("TRUE", "FALSE"), 
-                  ifelse(as.logical(tidy.opt$keep.comment), 1, 2), 
+    r.kc = gradio(c("TRUE", "FALSE"),
+                  ifelse(as.logical(tidy.opts$keep.comment), 1, 2),
                   horizontal = TRUE, container = gf.kc)
     tbl[2, 1, expand = TRUE] = (gf.kb <- gframe("Keep Blank Lines?", container = tbl))
-    r.kb = gradio(c("TRUE", "FALSE"), 
-                  ifelse(as.logical(tidy.opt$keep.blank.line), 1, 2), 
+    r.kb = gradio(c("TRUE", "FALSE"),
+                  ifelse(as.logical(tidy.opts$keep.blank.line), 1, 2),
                   horizontal = TRUE, container = gf.kb)
     tbl[3, 1, expand = TRUE] = (gf.ks <- gframe("Keep Spaces?", container = tbl))
-    r.ks = gradio(c("TRUE", "FALSE"), 
-                  ifelse(as.logical(tidy.opt$keep.space), 1, 2),
+    r.ks = gradio(c("TRUE", "FALSE"),
+                  ifelse(as.logical(tidy.opts$keep.space), 1, 2),
                   horizontal = TRUE, container = gf.ks)
     tbl[4, 1, expand = TRUE] = (gf.ra <- gframe("Replace '=' with '<-' in assigning operations?",
                                                 container = tbl))
-    r.ra = gradio(c("TRUE", "FALSE"), 
-                  ifelse(as.logical(tidy.opt$replace.assign), 1, 2),
+    r.ra = gradio(c("TRUE", "FALSE"),
+                  ifelse(as.logical(tidy.opts$replace.assign), 1, 2),
                   horizontal = TRUE, container = gf.ra)
     tbl[5, 1, expand = TRUE] = (gf.wi <- gframe("Text Width", container = tbl))
-    r.wi = gedit(as.character(tidy.opt$width.cutoff), container = gf.wi,
+    r.wi = gedit(as.character(tidy.opts$width.cutoff), container = gf.wi,
                  coerce.with = as.integer)
     g1 = ggroup(container = g)
     b.ok = gbutton("OK", container = g1, handler = function(h, ...) {
       if (is.na(svalue(r.wi))) {
         gmessage("Please input an integer for the text width!", "Error", icon = "error")
       } else {
-        tag(txt, "tidy.opt") = list(keep.comment = as.logical(svalue(r.kc)),
+        tidy.opts <<- list(keep.comment = as.logical(svalue(r.kc)),
                                     keep.blank.line = as.logical(svalue(r.kb)),
                                     keep.space = as.logical(svalue(r.ks)),
                                     replace.assign = as.logical(svalue(r.ra)),
