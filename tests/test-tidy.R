eef9834d31a9500faa17284501840b464627db35
Yihui Xie <xie@yihui.name>
2013-06-27 00:12:59 -0700

    use testit to run tests

--- tests/test-tidy.R
+++ tests/test-tidy.R
@@ -1,9 +1,11 @@
-library(formatR)
+library(testit)
+
 tidy.res = function(x, ...) {
   tidy.source(text = x, ..., output = FALSE)$text.tidy
 }
 
-stopifnot(
+assert(
+  'tidy.source() tries to keep R comments',
   identical(tidy.res('1+1#asdf'), '1 + 1  #asdf'),
   identical(tidy.res('paste(1 #asdf\n,2)'), 'paste(1  #asdf\n, 2)'),
   identical(tidy.res(c('# asdf', '1+1')), c('# asdf', '1 + 1'))
--- tests/test-utils.R
+++ tests/test-utils.R
@@ -1,23 +1,23 @@
-library(formatR)
+library(testit)
 
-with(getNamespace('formatR'), {
-  stopifnot(
-    # no indent before abc, so no indent before {
-    identical(move_leftbrace('abc() {\n    }'), 'abc()\n{\n    }'),
-    # 3 spaces before abc, 3 before {
-    identical(move_leftbrace('   a() {\n}'), c('   a()\n   {\n}')),
-    # blank lines are not removed
-    identical(move_leftbrace(c('a', '', 'b')), c('a', '', 'b')),
-    identical(move_leftbrace('if (TRUE) {\n  if (FALSE) {\n    1\n  }\n}'),
-              'if (TRUE)\n{\n  if (FALSE)\n  {\n    1\n  }\n}'),
-    identical(move_leftbrace('if (TRUE) {\n  1\n} else {\n  2}'),
-              'if (TRUE)\n{\n  1\n} else\n{\n  2}')
-  )
+assert(
+  'move_leftbrace() works',
+  # no indent before abc, so no indent before {
+  identical(move_leftbrace('abc() {\n    }'), 'abc()\n{\n    }'),
+  # 3 spaces before abc, 3 before {
+  identical(move_leftbrace('   a() {\n}'), c('   a()\n   {\n}')),
+  # blank lines are not removed
+  identical(move_leftbrace(c('a', '', 'b')), c('a', '', 'b')),
+  identical(move_leftbrace('if (TRUE) {\n  if (FALSE) {\n    1\n  }\n}'),
+            'if (TRUE)\n{\n  if (FALSE)\n  {\n    1\n  }\n}'),
+  identical(move_leftbrace('if (TRUE) {\n  1\n} else {\n  2}'),
+            'if (TRUE)\n{\n  1\n} else\n{\n  2}')
+)
 
-  stopifnot(
-    identical(reindent_lines(''), ''),
-    identical(reindent_lines(c('', '')), c('', '')),
-    identical(reindent_lines('    ', 2), '  '),
-    identical(reindent_lines('if (TRUE) {\n    1\n}', 2), 'if (TRUE) {\n  1\n}')
-  )
-})
+assert(
+  'reindent_lines() works',
+  identical(reindent_lines(''), ''),
+  identical(reindent_lines(c('', '')), c('', '')),
+  identical(reindent_lines('    ', 2), '  '),
+  identical(reindent_lines('if (TRUE) {\n    1\n}', 2), 'if (TRUE) {\n  1\n}')
+)
