0fb521ed96946a8358eac60d26d02b016c00f952
Serene-Arc <33189705+Serene-Arc@users.noreply.github.com>
2024-04-06 16:18:46 +1000

    Fix settings

--- .Rprofile
+++ .Rprofile
@@ -1,1 +1,2 @@
+Sys.setenv(RENV_CONFIG_AUTO_SNAPSHOT = "FALSE")
 source("renv/activate.R")
--- scripts/pre-commit-formatR.R
+++ scripts/pre-commit-formatR.R
@@ -1,34 +1,47 @@
 #!/usr/bin/env Rscript
 
-library("R.utils")
-library("formatR")
 
-defaults <- c(
-    source = "", comment = TRUE, blank = TRUE, arrow = FALSE, pipe = FALSE,
-    brace.newline = FALSE, indent = 4, wrap = TRUE, width.cutoff = 80,
-    args.newline = FALSE,
-)
+library(docopt)
+library(formatR)
 
-args <- cmdArgs()
-str(args)
+doc <- "
+Usage:
+  pre-commit-formatR.R [options] <files>...
 
-if ("--no_comments" %in% args)
-{
-    comments <- FALSE
-} else
-{
-    comments <- TRUE
+Options:
+  --no-comments         Remove comments. [default: FALSE]
+  --no-blanks           Remove blank lines. [default: FALSE]
+  --arrow               If FALSE, arrows will not be substituted for = signs. [default: FALSE]
+  --pipe                If FALSE, pipes will not be changed. [default: FALSE]
+  --brace-newline       If FALSE, braces will not be put on new lines. [default: FALSE]
+  --indent=INDENT       Number of indents. [default: 4]
+  --no-wrap             Skip wrapping the text. [default: TRUE]
+  --width-cutoff=WIDTH  Text width cutoff. [default: 80]
+  --args-newline        If TRUE, arguments are started on a new line after a function call. [default: FALSE]
+"
+
+args <- docopt(doc)
+
+source_files <- args[['files']]
+
+# get the width cutoff as integer
+width_cutoff <- as.integer(args[['--width-cutoff']])
+
+fileName_results = c()
+
+for(source_file in source_files){
+  tidy_result <- formatR::tidy_source(
+    source = source_file,
+    file = source_file,
+    comment = !args[['--no-comments']],
+    blank = !args[['--no-blanks']],
+    arrow = args[['--arrow']],
+    pipe = args[['--pipe']],
+    brace.newline = args[['--brace-newline']],
+    indent = as.integer(args[['--indent']]), 
+    wrap = !args[['--no-wrap']],
+    width.cutoff = as.integer(args[['--width-cutoff']]),
+    args.newline = args[['--args-newline']]
+  )$text
+
 }
-
-if ("--blank=TRUE" %in% args)
-{
-    blank <- TRUE
-} else
-{
-    blank <- FALSE
-}
-
-files <- dir(pattern = "*.R$")
-lapply(
-    files, function(f) formatR::tidy_source(f, comment = comments, blank = blank)
-)
